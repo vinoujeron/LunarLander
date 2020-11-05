04e05ef7406d16e09fc57e2abee0aa54e5619dec
unknown <vinoujeron@gmail.com>
2020-11-06 01:57:06 +0200

    Add player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private PlayerResourses playerResourses = null;
+    
+    [Header("Lander settings")]
+    [Range(0f, 180f)] [SerializeField] private float maxRotationAngle = 15f;
+    [Range(0f, 1000f)] [SerializeField] private float thrustPower = 10f;
+    [Range(0f, 1000f)] [SerializeField] private float rotatePushPower = 10f;
+    [SerializeField] private Rigidbody2D _rigidbody2D = null;
+
+    private void Update()
+    {
+        ManageInput();
+    }
+
+    private void ManageInput()
+    {
+        if (Input.GetKey(KeyCode.Space) && playerResourses.IsFuelLeft())
+            ApplyThrust(thrustPower);
+    }
+    private void FixedUpdate()
+    {
+        ApplyRotationBoost(Input.GetAxis("Horizontal"));
+    }
+
+    private void ApplyThrust(float _thrustPower)
+    {
+        playerResourses.ConsumeFuel();
+        _rigidbody2D.AddRelativeForce(new Vector2(0f, _thrustPower));
+    }
+    
+    private void ApplyRotationBoost(float inputAxis)
+    {
+        _rigidbody2D.AddForce(new Vector2(inputAxis * rotatePushPower, 0f));
+        transform.rotation = Quaternion.Euler(0f, 0f, inputAxis * maxRotationAngle);
+    }
+}
---
+++ Assets/Scripts/PlayerResourses.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerResourses : MonoBehaviour
+{
+    [Range(0f, 300f)] [SerializeField] private float fuelConsumptionPerSecond = 0f;
+
+    [Range(0f, 5000f)] [SerializeField] private float fuelCapacity = 0f;
+
+    public bool IsFuelLeft()
+    {
+        return fuelCapacity - fuelConsumptionPerSecond * Time.deltaTime >= 0;
+    }
+    public void ConsumeFuel()
+    {
+        if (fuelCapacity - fuelConsumptionPerSecond * Time.deltaTime >= 0) 
+            fuelCapacity -= fuelConsumptionPerSecond * Time.deltaTime;
+    }
+}
